a27e89a4fd15f48fe171d1bf56d0ea734df3c1f5
Naveduran <naveduran@gmail.com>
2021-02-04 02:03:50 +0000

    adding the 5 file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
