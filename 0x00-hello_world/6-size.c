4c54512356b4a674f19924ce15b1857a61f304db
Naveduran <naveduran@gmail.com>
2021-02-04 02:18:00 +0000

    updating the 6 file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+
+int main(void)
+{
+  printf ("Size of a char: 1 byte(s)\n");
+  printf ("Size of an int: 4 byte(s)\n");
+  printf ("Size of a long int: 4 byte(s)\n");
+  printf ("Size of a long long int: 8 byte(s)\n");
+  printf ("Size of a float: 4 byte(s)\n");
+return (0);
+}
