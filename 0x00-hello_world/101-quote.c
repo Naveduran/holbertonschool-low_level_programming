4591a73b96bab0bb1a1061d013c5c4dcc1c2cdde
Naveduran <naveduran@gmail.com>
2021-02-04 04:41:28 +0000

    adding the files 100 and 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#includu <st.h>
+
+/**
+ * main - Print the size of different variables in c
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+	      59);
+return (0);
+}
