835ebdb98819e8c435a248e72a8f5518521e1403
Naveduran <naveduran@gmail.com>
2021-02-04 01:24:06 +0000

    updating the 4 file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
-/*This is a c file */
+/**This is the first comment */
 #include <stdio.h>
+
+/**This is the second comment */
 
 int main(void)
 {
+
+/*this is the third comment*/
 puts("\"Programming is like building a multilingual puzzle");
+
+/*this is the fourth comment*/
 return (0);
 }
