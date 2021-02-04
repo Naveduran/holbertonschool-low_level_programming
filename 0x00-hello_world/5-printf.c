b4e2b78d85e425e444f11ffbceb87af336bc535f
Naveduran <naveduran@gmail.com>
2021-02-04 02:05:49 +0000

    adding the 5 file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -13,6 +12,6 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
