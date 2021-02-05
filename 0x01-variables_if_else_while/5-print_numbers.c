39679b34dbc87217a28f4412aaab8e10ff3ec9a3
Naveduran <naveduran@gmail.com>
2021-02-05 01:24:42 +0000

    updating the 5 file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabeth in lower
+ * and upper case.
+ *
+ * Return: always zero.
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
