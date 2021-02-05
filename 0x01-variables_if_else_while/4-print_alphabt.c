4af301ef46857da01874592c402e33eab0d67ecf
Naveduran <naveduran@gmail.com>
2021-02-05 00:39:05 +0000

    updating the file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabeth in lower
+ * and upper case.
+ *
+ * Return: always zero.
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	if (i == 'e')
+	{}
+	else if (i == 'q')
+	{}
+	else
+	{
+	putchar(i);
+	};
+	};
+	putchar('\b');
+	return (0);
+}
+
