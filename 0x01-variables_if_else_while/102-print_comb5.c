eb711ce763a0939276e8b7a0566f181f6efe3a16
Naveduran <naveduran@gmail.com>
2021-02-08 17:48:12 +0000

    adding files

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabeth in lower
+ * and upper case.
+ *
+ * Return: always zero.
+ */
+
+int main(void)
+{
+	int n;
+
+	printf("nothing");
+
+	return (0);
+}
