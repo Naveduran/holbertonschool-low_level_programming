4c26796c5fbf1e1e30f0fcf2b6b2325ae7246d83
Naveduran <naveduran@gmail.com>
2021-02-09 05:16:33 +0000

    giving content to 5.. file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,29 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - desc
+ * print_sign - prints if a number is positive or negative
+ * @n: is an int
  *
- * Return: desc
+ * Return: 1 and prints + if n is greater than zero;
+ * 0 and prints 0 if n is zero; and
+ * -1 and prints - if n is less than zero
  */
 
-int main(void)
+int print_sign(int n)
 {
-	printf("Holberton\n");
-	return (0);
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
