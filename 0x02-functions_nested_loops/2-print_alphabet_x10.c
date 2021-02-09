d44b64b41b3a5f0ccdd55adadaa4b5faec2ad3b0
Naveduran <naveduran@gmail.com>
2021-02-09 05:29:17 +0000

    2alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,21 @@
 
 /**
  * print_alphabet_x10 - prints the alphabet in lowercase
+ *
  */
 
 void print_alphabet_x10(void)
 {
-	int number = 97, counter = 0;
+	int number = 97;
+
+	int counter = 0;
 
 	while (counter <= 10)
 	{
 		while (number <= 'z')
 		{
-		_putchar(number);
-		number++;
+			_putchar(number);
+			number++;
 		}
 		number = 'a';
 		_putchar(10);
