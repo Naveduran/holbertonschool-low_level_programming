fcf6b4e067dee0417dadfa559e14f8c50078c341
Naveduran <naveduran@gmail.com>
2021-02-09 10:22:29 +0000

    adding content to the file 8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,29 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - desc
+ * jack_bauer - prints the alphabet in lowercase
  *
- * Return: desc
  */
 
-int main(void)
+void jack_bauer(void)
 {
-	printf("Holberton\n");
-	return (0);
+char d = 0;
+char c = 0;
+char b = 0;
+char a = 0;
+
+for (a = 48; a <= 50; a++)
+	for (b = 48; b <= 57; b++)
+		for (c = 48; c <= 53 ; c++)
+			for (d = 48; d <= 57; d++)
+			{
+				if (a == 50 && b == 52 && c == 48 && d == 48)
+					break;
+			_putchar(a);
+			_putchar(b);
+			_putchar(58);
+			_putchar(c);
+			_putchar(d);
+			_putchar(10);
+			}
 }
