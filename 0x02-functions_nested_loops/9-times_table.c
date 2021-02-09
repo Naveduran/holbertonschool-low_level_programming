705b4ea02f5a37dd4a5ac059a00818bc63ad07ab
Naveduran <naveduran@gmail.com>
2021-02-09 22:09:54 +0000

    adding the multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,40 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - desc
+ * times_table - prints the 9 times table
  *
- * Return: desc
  */
 
-int main(void)
+void times_table(void)
 {
-	printf("Holberton\n");
-	return (0);
+int fila = 0;
+int columna = 0;
+int result;
+int u;
+int d;
+for (fila = 0; fila <= 9; fila++)
+{
+	for (columna = 0; columna <= 9; columna++)
+	{
+		result = fila * columna;
+		if (result > 9)
+		{
+			u = result % 10;
+			d = result / 10;
+			_putchar(d + '0');
+			_putchar(u + '0');
+		}
+		else
+		{
+			_putchar(32);
+			_putchar(result + '0');
+		}
+		if (columna != 9)
+		{
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	_putchar(10);
+}
 }
