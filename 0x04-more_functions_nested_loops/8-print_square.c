f73d0e949337e33fc9d86431df1054e93a01da6b
Naveduran <naveduran@gmail.com>
2021-02-12 23:41:13 +0000

    add the file print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,10 @@ void print_square(int size)
 {
 	int columna, fila;
 
+
+	if (size <= 0)
+		_putchar(10);
+	else
 		for (fila = 0; fila < size; fila++)
 		{
 			for (columna = 0; columna < size; columna++)
