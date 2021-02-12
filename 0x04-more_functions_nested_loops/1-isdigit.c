5f95a6e6d264b3433ca983d87ffb9ec813a14dac
Naveduran <naveduran@gmail.com>
2021-02-12 02:52:49 +0000

    is digit added

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * _isdigit -  *
- * @c is an integer
+ * _isdigit - check if ant integer is a number *
+ * @c: is an integer
  *
  * Return: 1 if the int is a number from 0 to 9.
  * other wise it returns zero.
@@ -10,7 +10,7 @@
 
 int _isdigit(int c)
 {
-	if(c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
