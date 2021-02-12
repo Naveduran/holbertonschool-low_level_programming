dd4f1bc4a672c0e3ffe0a60ca9d441ea3ad2ca58
Naveduran <naveduran@gmail.com>
2021-02-12 02:45:11 +0000

    fixing the style of isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,16 @@
 
 /**
  * _isupper -  checks for uppercase character *
- *
- *
+ * @c: it is any int
+ * Return: 1 if the int is an upper case character,
+ * otherwise returns 0
  */
 
 int _isupper(int c)
 {
 
-	if (c => 65 && c <= 90)
-		return(1);
+	if (c >= 65 && c <= 90)
+		return (1);
 	else
 		return (0);
 }
