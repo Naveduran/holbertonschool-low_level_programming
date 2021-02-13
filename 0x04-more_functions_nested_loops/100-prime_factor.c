54c7e17cd0e5bc2e07add22b1b3169cbe7959eda
Naveduran <naveduran@gmail.com>
2021-02-13 03:52:11 +0000

    prints the largest cousin number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints finds and prints the largest prime factor of the number
+ * 612852475143
+ * Return: always zero.
+ *
+ */
+
+int main(void)
+{
+	unsigned long number = 612852475143;
+	int counter;
+	unsigned long imprimir;
+
+	for (counter = 2; number > 1; counter ++)
+	{
+		if ((number % counter) == 0)
+		{
+			number = number / counter;
+			imprimir = counter;
+			counter = 1;
+		}
+	}
+	printf("%lu\n", imprimir);
+	return (0);
+}
