a2578c5a9542a597c673e7d9aa6cb06b50822aba
Naveduran <naveduran@gmail.com>
2021-02-17 02:52:18 +0000

    printing only even numbers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
 /**
- * -
- * @n:
+ * puts2 - prints every other character of a string, starting with the first
+ * character, followed by a new line
+ * @str: the string to be printed
  *
  **/
 
-
+void puts2(char *str)
 {
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		_putchar(str[counter]);
+		counter = counter +2;
+	}
+	_putchar(10);
 
 }
