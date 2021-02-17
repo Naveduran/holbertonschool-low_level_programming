fe051c21220bae91b8396f4f72ac91708efb2b23
Naveduran <naveduran@gmail.com>
2021-02-17 02:08:41 +0000

    reviewing the checkers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,20 @@
 
 /**
  * print_rev - reverses a string.
- * @s: the original string
+ * @s: the first position of the original string
  **/
 
 void print_rev(char *s)
 {
-	int counter = 1;
-	for (; *s != '\0'; s++)
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
 	{
 		counter++;
 	}
-	for (; counter != 0; s--)
+	counter++;
+
+	for (; counter != 1; s--)
 	{
 		_putchar(*s);
 		counter--;
