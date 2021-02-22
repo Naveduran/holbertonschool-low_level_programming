6926d222935999020bb0cfa40728466d6b0bda93
Naveduran <naveduran@gmail.com>
2021-02-22 23:16:44 +0000

    printing the chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: pointer
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
