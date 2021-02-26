6f414e69d6f83eaf83c2fbc0bd3a9e1babfd240a
Naveduran <naveduran@gmail.com>
2021-02-26 12:25:07 +0000

    add file that prints the arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argv: values
+ * @argc: counter of values
+ * Return: always zero.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
