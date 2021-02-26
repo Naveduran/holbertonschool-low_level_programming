a7aa81f7ede1095e2f0e19cb0db4181534751eff
Naveduran <naveduran@gmail.com>
2021-02-26 11:44:05 +0000

    add print my new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - entry
+ * @argv: values
+ * @argc: counter
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
