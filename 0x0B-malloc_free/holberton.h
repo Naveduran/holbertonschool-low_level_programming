8ad28572dab830cc0aa60610157593e94a3167d4
Naveduran <naveduran@gmail.com>
2021-03-03 04:10:45 +0000

    README.md

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+void simple_print_buffer(char *buffer, unsigned int size);
+
+char *create_array(unsigned int size, char c);
+
+void print_grid(int **grid, int width, int height);
+
+char *_strdup(char *str);
+
+void print_grid(int **grid, int width, int height);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+#endif /* HOLBERTON_H */
