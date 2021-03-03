a46dfe5f134c81360dbeaf13c8054d593482400f
Naveduran <naveduran@gmail.com>
2021-03-03 04:32:33 +0000

    add a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,11 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	if (s1 != NULL)
-		for (l1 = 0; *(s1 + l1); l1++);
+		for (l1 = 0; *(s1 + l1); l1++)
+			;
 	if (s2 != NULL)
-		for (l2 = 0; *(s2 + l2); l2++);
+		for (l2 = 0; *(s2 + l2); l2++)
+			;
 
 	dino = malloc((sizeof(char) * (l1 + l2)) + 1);
 
@@ -26,7 +28,10 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < l1; i++)
 		*(dino + i) = *(s1 + i);
+
 	for (j = 0; j < l2; j++, i++)
+
 		*(dino + i) = *(s2 + j);
+
 	return (dino);
 }
