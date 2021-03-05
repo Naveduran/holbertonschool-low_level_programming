cda20370aa158f712ac34801124319924032eff2
Naveduran <naveduran@gmail.com>
2021-03-05 12:00:18 +0000

    [add] file that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,43 +14,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int max1, max2, maxtotal, counter;
 
 	if (*s1 == '\0')
-		return(0);
+		return (0);
 	max1 = strlen(s1);
 	if (n >= strlen(s2))
-	{
 		max2 = strlen(s2);
-	}
 	if (n < strlen(s2))
-	{
 		max2 = n;
-	}
 	maxtotal = max1 + max2 + 1;
-
 	pointer = malloc(maxtotal);
-
 	if (pointer == NULL)
-	{
-		return(NULL);
-	}
-
+		return (NULL);
 	for (counter = 0; counter <= maxtotal; counter++)
 	{
 
 		if (counter < max1)
-		{
 			pointer[counter] = s1[counter];
-			printf("con");
-		}
 		if (counter >= max1)
-		{
 			pointer[counter] = s2[counter - max1];
-			printf("din");
-		}
 		if (counter == maxtotal)
-		{
 			pointer[counter] = '\0';
-			printf("cat");
-		}
 	}
 	return (pointer);
 }
