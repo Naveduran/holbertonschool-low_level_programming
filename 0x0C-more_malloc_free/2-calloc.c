1af9770414f5da92be2602edd7bb1dcc7c49ec51
Naveduran <naveduran@gmail.com>
2021-03-05 19:41:25 +0000

    [Updated] Checking for prove cases

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,14 +13,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int counter;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
-	pointer = malloc(size * nmemb);
-
-	if (pointer == NULL)
-		return (NULL);
-	for (counter = 0; counter < size; counter++)
+		return ('\0');
+	else
 	{
-		pointer[counter] = '\0';
+		pointer = malloc(size * nmemb);
+
+		if (pointer == NULL)
+			return (NULL);
+		for (counter = 0; counter <= size; counter++)
+		{
+			pointer[counter] = '0';
+		}
+		return (pointer);
 	}
-	return (pointer);
 }
