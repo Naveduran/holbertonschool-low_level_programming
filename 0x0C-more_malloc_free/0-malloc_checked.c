e8392a21711878c9264c6294df58313bce41874f
Naveduran <naveduran@gmail.com>
2021-03-05 01:05:43 +0000

    [updated] betty style of file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,17 @@
 #include "holberton.h"
-
 /**
  * *malloc_checked - ..
  * @b: ..
- *
  * Return: ..
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *pointer;
 
 	pointer = malloc(b);
-
 	if (pointer == NULL)
 	{
 		exit(98);
 	}
-	return(pointer);
+	return (pointer);
 }
