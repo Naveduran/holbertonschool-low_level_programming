b22eff415ae98748131d64a6b390995897f964c3
Naveduran <naveduran@gmail.com>
2021-03-07 21:03:32 +0000

    [add] a file to create an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,14 +8,27 @@
 int *array_range(int min, int max)
 {
 	int *array;
+	int counter = 0;
+	int size;
 
 	if (min > max)
+	{
+/*		printf("min es mayor que max"); */
 		return (NULL);
+	}
 
-	array = malloc (max - min);
+	size = max - min;
+	array = malloc(size * 4);
 
 	if (array == NULL)
+	{
+/*		printf("no se pudo crear el array en la memoria"); */
 		return (NULL);
+	}
 
+	for (counter = 0; counter <= (max - min); counter++)
+	{
+		array[counter] = min + counter;
+	}
 	return (array);
 }
