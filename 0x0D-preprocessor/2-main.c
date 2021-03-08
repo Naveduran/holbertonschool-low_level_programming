daf0edf190992563898c84b59d5b6901b172f059
Naveduran <naveduran@gmail.com>
2021-03-08 21:08:41 +0000

    [Add] a program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled from.
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
