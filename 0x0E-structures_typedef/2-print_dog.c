f6aefb4a948a3334fa37afca66a3510e22cb0b93
Naveduran <naveduran@gmail.com>
2021-03-09 14:27:15 +0000

    [Add] a function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: ..
+ */
+
+void print_dog(struct dog *d)
+{
+
+	printf("Name: %s\n Age: %.1f\n Owner %s\n", my_dog.name,
+my_dog.age, my_dog.owner);
+
+}
