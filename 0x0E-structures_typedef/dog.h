4d11338e28862d9d3714874bf08876ed13df433b
Naveduran <naveduran@gmail.com>
2021-03-09 02:58:56 +0000

    [Updated] Added the prototypes of all the project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
 /**
  * struct dog - ..
  * @name: ..
@@ -15,5 +16,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /* HOLBERTON_H */
