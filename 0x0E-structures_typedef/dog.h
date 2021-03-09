4190360a410976064cb73893537f9436573c2735
Naveduran <naveduran@gmail.com>
2021-03-09 02:12:13 +0000

    [Add] a header with a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - ..
+ * @name: ..
+ * @age: ..
+ * @owner: ..
+ * Description: ..
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HOLBERTON_H */
