9aa54e166cfd222e2ec3f122fb3bc828ec55fcb5
Naveduran <naveduran@gmail.com>
2021-03-19 10:52:16 +0000

    [Updated] behavior when null

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		length++;
 		h = h->next;
