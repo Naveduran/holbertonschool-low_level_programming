c42e573c3caee534d8bea16689b976256ed12cd4
Naveduran <naveduran@gmail.com>
2021-03-19 10:29:35 +0000

    [Updated] conditions to keep iterating

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t length = 1;
+	size_t length = 0;
 
 	if (!h)
 	{
@@ -15,7 +15,12 @@ size_t print_list(const list_t *h)
 	}
 	while (h)
 	{
-		printf("[%d] (%s)\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
 		length++;
 		h = h->next;
 	}
