18a341662a8ac4d72c382c628cb82e89ab2b69ab
Naveduran <naveduran@gmail.com>
2021-03-23 11:49:55 +0000

    [Updated] last node NULL assignation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temporary;
 
-	while (*head)
-	{
-		temporary = (*head)->next;
-		free(*head);
-		(*head) = temporary;
-	}
-	head = NULL;
+	if (head != NULL)
+		while (*head)
+		{
+			temporary = (*head)->next;
+			free(*head);
+			(*head) = temporary;
+		}
 }
