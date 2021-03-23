f764e25b0edd8377f53c1a24dee093e0056ac748
Naveduran <naveduran@gmail.com>
2021-03-23 17:32:29 +0000

    [Updated] behavior when idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,8 +3,7 @@
  * *insert_nodeint_at_index - a function that inserts a new node at a given
  * position.
  * @head: a pointer to the head node of the list
- * @idx: the index of the list where the new node should be added.
- * Index starts at 0
+ * @idx: the index of the list where the new node should be added, starts at 0.
  * @n: data to assign at the new node
  * Return: the address of the new node, or NULL if it failed
  */
@@ -13,31 +12,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temporary_head, *temporary_next;
 	unsigned int counter;
 
-/* if there are not parameters return NULL and stop*/
 	if (!head || !idx || !n)
 		return (NULL);
-/*allocate space of memory for the new node and confirm it is done*/
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n, new_node->next = *head;
+		*head = new_node, return (new_node);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-/*save the head in temporary variable*/
 	temporary_head = *head;
-/*walk through the list until you reach the index minus one */
 	for (counter = 0; counter < (idx - 1); counter++)
 	{
 		*head = (*head)->next;
 		if (*head == NULL)
 			return (NULL);
 	}
-/*save temporarily the direction saved in the next position to assign later*/
 	temporary_next = (*head)->next;
-/*assign the direction of the new node to the next of the previus node*/
-	(*head)->next = new_node;
-/*assign the data to be stored in the new_node*/
-	new_node->n = n;
-/*assign the direction of the next of the new node*/
-	new_node->next = temporary_next;
-/*assign temporary back to the head variable */
-	*head = temporary_head;
+	(*head)->next = new_node, new_node->n = n;
+	new_node->next = temporary_next, *head = temporary_head;
 	return (new_node);
 }
