6affa272cdb9e52f5873f6599a1d2579da8f2777
Naveduran <naveduran@gmail.com>
2021-03-23 17:51:54 +0000

    [Updated] behavior when idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,14 +14,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head || !*head)
 		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
 	if (idx == 0)
 	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+		return (NULL);
 		new_node->n = n, new_node->next = *head, *head = new_node;
 		return (new_node);
 	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 	temporary_head = *head;
 	for (counter = 0; counter < (idx - 1); counter++)
 	{
