86eadacf66e9e18e2fe6fb75140574266da765b7
Naveduran <naveduran@gmail.com>
2021-03-23 18:47:09 +0000

    [Updated] Order of the process

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,25 +12,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temporary_head, *temporary_next;
 	unsigned int counter;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
-	if (idx == 0)
-	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-		return (NULL);
-		new_node->n = n, new_node->next = *head, *head = new_node;
-		return (new_node);
-	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	temporary_head = *head;
-	for (counter = 0; counter < (idx - 1); counter++)
+	if (idx == 0)
 	{
-		*head = (*head)->next;
+		new_node->n = n, new_node->next = *head, *head = new_node;
+		return (new_node);
+	}
+	temporary_head = *head;
+	for (counter = 0; counter < (idx - 1)/**/; counter++)
+	{
 		if (*head == NULL)
 			return (NULL);
+		*head = (*head)->next;
 	}
 	temporary_next = (*head)->next;
 	(*head)->next = new_node, new_node->n = n;
