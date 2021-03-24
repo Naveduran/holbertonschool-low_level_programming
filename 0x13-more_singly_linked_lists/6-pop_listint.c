a5d9ebdd2b7160ee2bcb726d22153d81310b958c
Naveduran <naveduran@gmail.com>
2021-03-24 14:31:39 +0000

    [Updated] behavior if head is equal to NULL

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,7 +12,7 @@ int pop_listint(listint_t **head)
 	listint_t *temporary;
 
 	if (head == NULL)
-		return (0);
+		return ('0');
 	node_data = (*head)->n;
 	temporary = (*head)->next;
 	free(*head);
