836342c7ec53466bca9d05c41d8a09da8ce8f49c
Naveduran <naveduran@gmail.com>
2021-03-25 15:13:30 +0000

    [Add] Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,7 @@
 	.file	"example.c"
 	.section	.rodata
 .LC0:
-	.string	"Hello, Holberton"
+	.string	"h"
 	.text
 	.globl	main
 	.type	main, @function
@@ -13,9 +13,11 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
+	movl	$1, %edx
+	movl	$.LC0, %esi
+	movl	$1, %edi
 	movl	$0, %eax
-	call	printf
+	call	write
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
