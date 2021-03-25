904fd427cc18a54f2a59e3fed00c1378b6e8e8f3
Naveduran <naveduran@gmail.com>
2021-03-25 15:57:23 +0000

    [AUpdated] another way to print a string in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,22 @@
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-main:
-.LFB0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-.LFE0:
-	.size	main, .-main
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+;; The string to be printed
+	STRING DB 'Hello, Holberton', '$'
+
+.CODE
+	MOV AX,@DATA
+	MOV DS,AX
+
+	;;  load address of the string
+	LEA DX,STRING
+
+	;; output the string
+	;; loaded in dx
+	MOV AH,09H
+	INT 21H
+
+	;; interrupt to exit
+	MOV AH,4CH
+	INT 21H
