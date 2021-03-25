5d4d5281782d128cc258b2accfd5885c219c1f10
Naveduran <naveduran@gmail.com>
2021-03-25 15:18:38 +0000

    [Add] Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,25 @@
 	.file	"example.c"
 	.section	.rodata
 .LC0:
-	.string	"h"
+	.string	"H"
+.LC1:
+	.string	"e"
+.LC2:
+	.string	"l"
+.LC3:
+	.string	"o"
+.LC4:
+	.string	","
+.LC5:
+	.string	"b"
+.LC6:
+	.string	"r"
+.LC7:
+	.string	"t"
+.LC8:
+	.string	"n"
+.LC9:
+	.string	"\n"
 	.text
 	.globl	main
 	.type	main, @function
@@ -15,6 +33,81 @@ main:
 	.cfi_def_cfa_register 6
 	movl	$1, %edx
 	movl	$.LC0, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC1, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC2, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC2, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC3, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC4, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC0, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC3, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC2, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC5, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC1, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC6, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC7, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC3, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC8, %esi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	write
+	movl	$1, %edx
+	movl	$.LC9, %esi
 	movl	$1, %edi
 	movl	$0, %eax
 	call	write
