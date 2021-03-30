fcd9b4ba8976d2087e806e5bf7f49768716b77cf
Naveduran <naveduran@gmail.com>
2021-03-30 06:51:42 +0000

    [Add] a header file with prototypes and libraries

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h> /*for */
+
+#include <unistd.h> /* for STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO,*/
+/*ssize_t, execve, write, read, isatty*/
+
+#include <sys/stat.h> /* for mkdir, chmod, stat*/
+
+#include <sys/types.h> /*for types of files*/
+
+#include <stdlib.h> /*for exit and malloc*/
+
+#include <fcntl.h> /*for O_CREAT, O_APPEND, O_RDONLY, O_RDWR, O_WRONLY,*/
+/* O_TRUNC*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTON_H */
