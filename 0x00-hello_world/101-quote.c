00d6c9c50f3b575089eea2ca51b065edeccd9238
Naveduran <naveduran@gmail.com>
2021-04-27 21:36:38 +0000

    using the c function write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
- * main - imprime string
- *
- * Return: devuelve un zero
+ * main - prints exactly and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard error.
+ * Return: zero
 */
 
 int main(void)
 {
-	fprintf(stderr,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(0);
+char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t nbytes;
+
+nbytes = strlen(buf);
+write(2, buf, nbytes);
+
+exit(1);
+return (1);
 }
