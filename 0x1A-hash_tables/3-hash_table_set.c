987cd935e95442053e48aca90bd685feb5da266d
Naveduran <naveduran@gmail.com>
2021-05-29 06:17:48 +0000

    conditionals and search

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,27 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * search_in_list - search a key in a linked list
- * @ht: pointer to hash_table
- * @key: key to search
- * @possition: possition in hash table
- * Return: 1 if match 0 if not
- */
-int search_in_list(hash_table_t *ht, const char *key, unsigned int possition)
-{
-	hash_node_t *initial_point = ht->array[possition];
-
-	for (; ht->array[possition];)
-	{
-		if ((strcmp(ht->array[possition]->key, key)) == 0)
-			return (0);
-		ht->array[possition] = ht->array[possition]->next;
-	}
-	ht->array[possition] = initial_point;
-	return (1);
-}
-
-/**
  * hash_table_set - a function that adds an element to the hash table.
  * @ht: he hash table you want to add or update
  * @key: key
@@ -31,25 +10,30 @@ int search_in_list(hash_table_t *ht, const char *key, unsigned int possition)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index = key_index((unsigned const char *) key, ht->size);
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *list_node = NULL;
 
+	/*check key*/
+	if (!key || key == NULL)
+		return (0);
 	/*if the table doesn't exist or is empty*/
-	if (!ht || ht->size < 1)
+	if (!ht || ht->size == 0)
 		return (0);
 
-	/*if the key already exist update the value*/
-	if ((search_in_list(ht, key, index)) == 0)
+	/*if the key already exist in any node of the list, update the value*/
+	list_node = ht->array[index];
+	while (list_node != NULL)
 	{
-		ht->array[index]->value = (char *) value;
-		return (1);
+		if (list_node->key == key)
+			list_node->value = (char *) value;
+		list_node = list_node->next;
 	}
 
 	/*create a new node */
 	new_node = create_new_node(key, value);
-
 	/*if that index has a node already, save the reference to the next*/
 	if (ht->array[index])
 		new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
