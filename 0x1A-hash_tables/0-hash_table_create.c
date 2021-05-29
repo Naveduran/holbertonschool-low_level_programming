8cfeeccf715526d89454a1b1e91042bae45bd3bd
Naveduran <naveduran@gmail.com>
2021-05-29 05:10:46 +0000

    a function to create a node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,6 @@
  * Return: pointer to the created structure
  */
 
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *htable = malloc(sizeof(hash_table_t));
@@ -21,4 +20,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	return (htable);
+}
+
+/**
+ * create_new_node - creates a new node, assign key and value
+ * @key: key
+ * @value: value
+ * Return: pointer to the new_node
+ */
+
+hash_node_t *create_new_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+/*create a new node */
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+/*assign value and key*/
+	new_node->value = strdup((char *) value);
+	new_node->key = strdup((char *) key);
+	new_node->next = NULL;
+	return (new_node);
 }
