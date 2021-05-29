3aa39071ccb71fd7903ca2bb61da094700df6728
Naveduran <naveduran@gmail.com>
2021-05-29 06:19:21 +0000

    create_node

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /*malloc and calloc*/
 #include <string.h>
 #include <unistd.h>
 
@@ -41,6 +41,8 @@ unsigned long int hash_djb2(const unsigned char *str);
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
+hash_node_t *create_new_node(const char *key, const char *value);
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 char *hash_table_get(const hash_table_t *ht, const char *key);
@@ -48,7 +50,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 
 void hash_table_delete(hash_table_t *ht);
-
-
 
 #endif /* HOLBERTON_H */
